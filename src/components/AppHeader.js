cde0647a18a9d5f32161cdcfc5c09ff1a4812b00
Atsuki Tanaka <atsuki_tanaka@icloud.com>
2022-07-28 14:55:07 +0900

    header color 変更

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
 const Header = styled.header`
   display: flex;
@@ -7,13 +8,14 @@ const Header = styled.header`
   height: 4em;
   position: fixed;
   align-items: center;
-  background-color: white;
+  background-color: #a6120d;
 `;
 
 const HeaderIcon = styled.div`
   margin-left: 1em;
   font-family: 'Stick', sans-serif;
   font-size: 1.2em;
+  color: white;
 
   font-weight: 600;
 `;
